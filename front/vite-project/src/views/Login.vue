8c5d0fab51777aee547e3877eb851b87adb6f6aa
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-21 23:02:35 +0900

    feat: 회원가입, 로그인 로직 구현

--- front/vite-project/src/views/Login.vue
+++ front/vite-project/src/views/Login.vue
@@ -8,20 +8,20 @@
 
       <div class="login__forms">
         <!-- login form -->
-        <form class="login__register login-in" v-if="!isBlock">
+        <form @submit.prevent="login" class="login__register login-in" v-if="!isBlock">
           <h1 class="login__title">Sign In</h1>
           <span>type your account</span>
           <div class="login__box">
             <i class='bx bx-user login__icon'></i>
-            <input type="text" placeholder="Login ID" class="login__input">
+            <input type="text" placeholder="Login ID" class="login__input" v-model="signInState.form.loginId">
           </div>
           <div class="login__box">
             <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password" class="login__input">
+            <input type="password" placeholder="Password" class="login__input" v-model="signInState.form.password">
           </div>
           <a href="#" class="login__forgot">Forgot Password? </a>
           
-          <a href="#" class="login__button">Sign In</a>
+          <button type="submit" class="login__button">Sign In</button>
           
           <div>
             <span class="login__account login__account--account">Don't Have an Account?</span>
@@ -30,30 +30,30 @@
         </form>
         
         <!-- create account form -->
-        <form class="login__create login-up" v-if="isBlock">
+        <form @submit.prevent="register" class="login__create login-up" v-if="isBlock">
           <h1 class="login__title">Create Account</h1>
           <span>type your info for registration</span>
           <div class="login__box">
             <i class='bx bx-user login__icon'></i>
-            <input type="text" placeholder="Login ID" class="login__input">
+            <input type="text" placeholder="Login ID" class="login__input" v-model="signUpState.form.loginId">
           </div>
           
           <div class="login__box">
             <i class='bx bx-pencil login__icon'></i>
-            <input type="text" placeholder="Nickname" class="login__input">
+            <input type="text" placeholder="Nickname" class="login__input" v-model="signUpState.form.nickName">
           </div>
           
           <div class="login__box">
             <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password" class="login__input">
+            <input type="password" placeholder="Password" class="login__input" v-model="signUpState.form.password">
           </div>
 
           <div class="login__box">
             <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password Confirm" class="login__input">
+            <input type="password" placeholder="Password Confirm" class="login__input" v-model="signUpState.form.passwordConfirm">
           </div>
           
-          <a href="#" class="login__button">Sign Up</a>
+          <button type="submit" class="login__button">Sign Up</button>
           
           <div>
             <span class="login__account login__account--account">Already have an Account?</span>
@@ -66,11 +66,22 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref, computed, watchEffect } from 'vue'
+import axios from 'axios'
+import router from '../router/index'
+import store from '../store/index'
 
 export default {
   name: 'Login',
   setup() {
+    const isAuthenticated = computed(() => store.state.isAuthenticated);
+
+    watchEffect(() => {
+      if(isAuthenticated.value) {
+        router.push({path: '/'});
+      }
+    })
+
     const isBlock = ref(false);
 
     /**
@@ -81,9 +92,71 @@ export default {
       isBlock.value = status;
     };
 
+    const signUpState = reactive({
+      form: {
+        loginId: "",
+        nickName: "",
+        password: "",
+        passwordConfirm: ""
+      },
+      message: "",
+    });
+
+    const signInState = reactive({
+      form: {
+        loginId: "",
+        password: "",
+      }
+    })
+
+    /**
+     * 회원가입
+     */
+    const register = () => {
+      axios.post('/api/join', signUpState.form).then((res) => {
+        signUpState.message = res.data.message;
+        window.alert(signUpState.message);
+        changeBlock(false); // 로그인으로 이동
+
+      }).catch((error) => {
+        if(error.response && error.response.data) {
+          signUpState.message = error.response.data.message;
+        } else {
+          signUpState.message = "사용할 수 없는 아이디 입니다.";
+        }
+
+        window.alert(signUpState.message);
+      });
+    };
+
+    /**
+     * 로그인
+     */
+    const login = () => {
+      axios.post('/api/login', signInState.form).then((res) => {
+        const accessToken = res.headers["authorization"];
+
+        if(accessToken) {
+          localStorage.setItem('accessToken', accessToken);
+          window.alert('로그인 성공!');
+          store.dispatch('initializeAuthentication');
+          router.push({path: '/'});
+        } else {
+          window.alert('로그인 토큰을 받아오는데 실패했습니다.');
+        }
+      }).catch(() => {
+        window.alert('아이디 혹은 비밀번호가 일치하지 않습니다..');
+      })
+    };
+
     return {
+      isAuthenticated,
       isBlock,
       changeBlock,
+      signUpState,
+      register,
+      signInState,
+      login
     }
   }
 }
