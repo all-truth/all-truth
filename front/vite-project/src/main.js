fa44f7cd4350ab9669ee5df80e23fb536a672c17
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-21 23:02:14 +0900

    refactor: Vuex 및 Vue Router를 애플리케이션에 추가

--- front/vite-project/src/main.js
+++ front/vite-project/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import store from './store/index'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
+
+store.dispatch('initializeAuthentication')  // 로그인 상태 초기화
 
 app.mount('#app')
