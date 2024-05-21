a007e95589b7a9991af6ea22df2610e2b343ca6e
SeungYn <kwls0407@gmail.com>
2024-05-21 16:14:18 +0900

    feat: User 엔티티 추가

---
+++ back/src/main/java/com/alltruth/api/entity/User.java
@@ -0,0 +1,40 @@
+package com.alltruth.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String loginId;
+    private String password;
+    private String roles; // User, Admin
+
+    public List<String> getRoleList(){
+        if(roles.length() > 0){
+            return Arrays.asList(roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    @Builder
+    public User(String loginId, String password, String roles){
+        this.loginId = loginId;
+        this.password = password;
+        this.roles = roles;
+    }
+
+
+}
