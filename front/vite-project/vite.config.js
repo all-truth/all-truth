39c1db2b219202e2b297021d57870e6dfa27c5ab
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-21 22:55:29 +0900

    fix: api와 라우팅 경로가 동일할 때 발생하는 문제 해결

--- front/vite-project/vite.config.js
+++ front/vite-project/vite.config.js
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      // 페이지 라우팅과 api URI가 동일한 문제를 rewrite로 구분
+      '/api/join': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/api/login': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
+});
