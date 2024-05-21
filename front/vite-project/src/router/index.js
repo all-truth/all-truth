b0f2b525392c12427cf4ff0e6a4afc6e2784e35c
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-21 16:17:39 +0900

    feat: Vue Router로 페이지 간 이동 구현

--- front/vite-project/src/router/index.js
+++ front/vite-project/src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Content from '../components/Content.vue'
-import Login from '../views/Login.vue'
-import Review from '../views/review/Review.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Content from '../components/Content.vue';
+import Login from '../views/Login.vue';
+import Review from '../views/review/Review.vue';
 
 const routes = [
   { path: '/', component: Content },
   { path: '/login', component: Login },
   { path: '/review/:id', component: Review },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: routes
-})
+  routes: routes,
+});
 
 export default router;
