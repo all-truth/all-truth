e4a51271b900cd95d5bcc4bc04e144e59c0b806e
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-23 17:57:44 +0900

    feat: 리뷰 조회 api 호출 및 응답 데이터 하위 컴포넌트에 전달

---
+++ front/vite-project/src/views/Reviews.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card shadow-sm">
+    <div class="card-body">
+      <span class="img" :style="{backgroundImage: `url(${review.images.length > 0 ? review.images[0] : '/default_review_img.png'})`}"></span>
+      <p class="card-text">{{ handleText.sliceText(review.title) }}</p>
+      <div class="d-flex justify-content-between align-items-center py-3">
+        <strong class="name">{{ review.storeName }}</strong>
+        <small class="text-body-secondary">{{ review.region }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import handleText from '../scripts/handleString'
+
+export default {
+  name: 'Review',
+  props: {
+    review: Object
+  },
+  setup() {
+
+    return {
+      handleText,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.name {
+  font-size: 15px;
+  color: cadetblue;
+}
+
+.img {
+  display: inline-block;
+  width: 100%;
+  height: 225px;
+  background-size: cover;
+  background-position: center;
+}
+
+</style>
