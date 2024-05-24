0f43a84ba6e628ca010ea37410aaff0f20504f71
SeungYn <kwls0407@gmail.com>
2024-05-24 17:52:22 +0900

    feat: 파일 관련 Exception 추가

---
+++ back/src/main/java/com/alltruth/api/config/common/exceptions/file/StorageFileNotFoundException.java
@@ -0,0 +1,14 @@
+package com.alltruth.api.config.common.exceptions.file;
+
+
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
