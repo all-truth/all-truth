7c61f143a3bf0819df5aad905311c778aaf68c3c
SeungYn <kwls0407@gmail.com>
2024-05-24 17:28:57 +0900

    fix: corsConfig 패턴 수정

--- back/src/main/java/com/alltruth/api/config/security/CorsConfig.java
+++ back/src/main/java/com/alltruth/api/config/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
