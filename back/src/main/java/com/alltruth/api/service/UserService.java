559f897040487d8db48b855a2c2138952d35875c
SeungYn <kwls0407@gmail.com>
2024-05-24 17:51:51 +0900

    feat: 회원가입 중복 처리 수정

--- back/src/main/java/com/alltruth/api/service/UserService.java
+++ back/src/main/java/com/alltruth/api/service/UserService.java
@@ -3,6 +3,7 @@ package com.alltruth.api.service;
 import com.alltruth.api.dto.UserDTO;
 import com.alltruth.api.entity.User;
 import com.alltruth.api.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,10 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Transactional
     public UserDTO.UserJoinRes join(UserDTO.UserJoinReq userJoinReq){
         User checkUser = userRepository.findByLoginId(userJoinReq.getLoginId()).orElse(null);
-
-        if(checkUser != null){
-            //throw new Error("아이디가 중복 됐습니다!");
-            return new UserDTO.UserJoinRes("아이디가 중복 됐습니다!");
-        }
+        if(checkUser != null) new IllegalArgumentException("아이디가 중복 됐습니다!");
 
         User user = User.builder()
                 .loginId(userJoinReq.getLoginId())
