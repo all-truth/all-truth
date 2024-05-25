530277f6d04aae386e26d3f8dcceab772b751f96
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-25 16:47:09 +0900

    feat: 댓글 컴포넌트 구현

---
+++ front/vite-project/src/views/review/Comment.vue
@@ -0,0 +1,35 @@
+<template>
+    <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#007bff"></rect>
+    </svg>
+    <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+      <div class="d-flex justify-content-between">
+        <strong class="text-gray-dark">{{ comment.nickName }}</strong>
+        <a href="#" v-if="user !== null && comment.userId === user.id">삭제</a>
+      </div>
+      <span class="d-block">{{ comment.content }}</span>
+    </div>
+</template>
+
+<script>
+import store from '../../store/index'
+
+export default {
+  name: 'Comment',
+  props: {
+    comment: Object
+  },
+  setup() {
+    const user = store.state.user;
+
+    return {
+      user,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
