56c5205d0e6483bd9b663d014d9623d5c6af42d6
SeungYn <kwls0407@gmail.com>
2024-05-26 18:07:24 +0900

    chore: gitIgnore 수정

--- back/src/main/java/com/alltruth/api/SeungynApplication.java
+++ back/src/main/java/com/alltruth/api/SeungynApplication.java
@@ -1,13 +1,52 @@
 package com.alltruth.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class SeungynApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		SpringApplication.run(SeungynApplication.class, args);
+		File file = new File("/upload");
+		file.mkdir();
+		System.out.println(file.getAbsoluteFile());
+		System.out.println("asd");
+		System.out.println(Path.of("upload").resolve(
+
+				"dsf"));
+
+		Path a = Path.of("upload","whiteboardappdotorg20210910235613.png");
+		System.out.println(a.toString());
+		Resource res = new UrlResource(a.toUri());
+		System.out.println(a.toUri());
+		System.out.println(res);
+
+		System.out.println("-------enum-------");
+		System.out.println(Test.TTTTT);
+		System.out.println(Test.TTTTT.name());
+		System.out.println(Test.TTTTT.getA());
+		System.out.println(Test.TTTTT.getB());
 	}
+
+}
+
+@AllArgsConstructor
+@Getter
+enum Test{
+	TTTTT("aaa", "bbb");
+
+	private String a;
+	private String b;
+
 
 }
--- back/src/main/java/com/alltruth/api/config/security/jwt/JwtTokenProvider.java
+++ back/src/main/java/com/alltruth/api/config/security/jwt/JwtTokenProvider.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,15 +16,15 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
-
+//
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
     private final JwtProperties jwtProperties;
 
-    public String generateToken(User user, Duration expriedAt){
+    public String generateToken(User user, Duration expiredAt){
         Date now = new Date();
-        return makeToken(new Date(now.getTime() + expriedAt.toMillis()), user);
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
     }
 
     private String makeToken(Date expiry, User user){
@@ -47,6 +48,7 @@ public class JwtTokenProvider {
                     .parseClaimsJws(token);
             return true;
         }catch(Exception e){
+            System.out.println("토큰파싱 실패");
             return false;
         }
     }
