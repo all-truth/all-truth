d316c26e4db5fe682bcd164cbd7772201fc1742c
SeungYn <kwls0407@gmail.com>
2024-05-26 17:13:58 +0900

    design: 로그인, 회원가입 UI 디자인

--- front/vite-project/src/components/Content.vue
+++ front/vite-project/src/components/Content.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="album py-5 bg-body-tertiary">
     <div class="container">
-
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <div class="col" v-for="review in state.reviews" :key="review.id">
           <Reviews :review="review" />
@@ -13,8 +12,8 @@
 
 <script>
 import { onMounted, reactive } from 'vue';
-import Reviews from '../views/review/Reviews.vue'
-import axios from 'axios'
+import Reviews from '../views/review/Reviews.vue';
+import axios from 'axios';
 
 export default {
   name: 'Content',
@@ -23,24 +22,25 @@ export default {
   },
   setup() {
     const state = reactive({
-      reviews: []
-    })
+      reviews: [],
+    });
 
     onMounted(() => {
-      axios.get('/api/reviews').then((res) => {
-        state.reviews = res.data;
-      }).catch((error) => {
-        console.error('리뷰 조회 중 에러가 발생했습니다. ', error);
-      });
+      axios
+        .get('/api/reviews')
+        .then((res) => {
+          state.reviews = res.data;
+        })
+        .catch((error) => {
+          console.error('리뷰 조회 중 에러가 발생했습니다. ', error);
+        });
     });
 
     return {
       state,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- front/vite-project/src/views/Login.vue
+++ front/vite-project/src/views/Login.vue
@@ -1,64 +1,118 @@
 <template>
   <div class="login">
     <div class="login__content">
-
       <div class="login__img">
-        <img src="https://image.freepik.com/free-vector/code-typing-concept-illustration_114360-3581.jpg" alt="user login">
+        <img
+          src="https://image.freepik.com/free-vector/code-typing-concept-illustration_114360-3581.jpg"
+          alt="user login"
+        />
       </div>
 
       <div class="login__forms">
         <!-- login form -->
-        <form @submit.prevent="login" class="login__register login-in" v-if="!isBlock">
+        <form
+          @submit.prevent="login"
+          class="login__register login-in"
+          v-if="!isBlock"
+        >
           <h1 class="login__title">Sign In</h1>
           <span>type your account</span>
           <div class="login__box">
-            <i class='bx bx-user login__icon'></i>
-            <input type="text" placeholder="Login ID" class="login__input" v-model="signInState.form.loginId">
+            <i class="bx bx-user login__icon"></i>
+            <input
+              type="text"
+              placeholder="Login ID"
+              class="login__input"
+              v-model="signInState.form.loginId"
+            />
           </div>
           <div class="login__box">
-            <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password" class="login__input" v-model="signInState.form.password">
+            <i class="bx bx-lock login__icon"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              class="login__input"
+              v-model="signInState.form.password"
+            />
           </div>
           <a href="#" class="login__forgot">Forgot Password? </a>
-          
+
           <button type="submit" class="login__button">Sign In</button>
-          
+
           <div>
-            <span class="login__account login__account--account">Don't Have an Account?</span>
-            <span class="login__signin login__signin--signup sign-up" @click="changeBlock(true)">Sign Up</span>
+            <span class="login__account login__account--account"
+              >Don't Have an Account?</span
+            >
+            <span
+              class="login__signin login__signin--signup sign-up"
+              @click="changeBlock(true)"
+              >Sign Up</span
+            >
           </div>
         </form>
-        
+
         <!-- create account form -->
-        <form @submit.prevent="register" class="login__create login-up" v-if="isBlock">
+        <form
+          @submit.prevent="register"
+          class="login__create login-up"
+          v-if="isBlock"
+        >
           <h1 class="login__title">Create Account</h1>
           <span>type your info for registration</span>
           <div class="login__box">
-            <i class='bx bx-user login__icon'></i>
-            <input type="text" placeholder="Login ID" class="login__input" v-model="signUpState.form.loginId">
-          </div>
-          
-          <div class="login__box">
-            <i class='bx bx-pencil login__icon'></i>
-            <input type="text" placeholder="Nickname" class="login__input" v-model="signUpState.form.nickname">
-          </div>
-          
-          <div class="login__box">
-            <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password" class="login__input" v-model="signUpState.form.password">
+            <i class="bx bx-user login__icon"></i>
+            <input
+              type="text"
+              placeholder="Login ID"
+              class="login__input"
+              v-model="signUpState.form.loginId"
+            />
           </div>
 
           <div class="login__box">
-            <i class='bx bx-lock login__icon'></i>
-            <input type="password" placeholder="Password Confirm" class="login__input" v-model="signUpState.form.passwordConfirm">
+            <i class="bx bx-pencil login__icon"></i>
+            <input
+              type="text"
+              placeholder="Nickname"
+              class="login__input"
+              v-model="signUpState.form.nickname"
+            />
           </div>
-          <small v-if="!isMatched" class="password__match">비밀번호가 일치하지 않습니다.</small>
-          
+
+          <div class="login__box">
+            <i class="bx bx-lock login__icon"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              class="login__input"
+              v-model="signUpState.form.password"
+            />
+          </div>
+
+          <div class="login__box">
+            <i class="bx bx-lock login__icon"></i>
+            <input
+              type="password"
+              placeholder="Password Confirm"
+              class="login__input"
+              v-model="signUpState.form.passwordConfirm"
+            />
+          </div>
+          <small v-if="!isMatched" class="password__match"
+            >비밀번호가 일치하지 않습니다.</small
+          >
+
           <button type="submit" class="login__button">Sign Up</button>
-          
+
           <div>
-            <span class="login__account login__account--account">Already have an Account?</span>
-            <span class="login__signup login__signup--signup sign-in" @click="changeBlock(false)">Sign In</span>
+            <span class="login__account login__account--account"
+              >Already have an Account?</span
+            >
+            <span
+              class="login__signup login__signup--signup sign-in"
+              @click="changeBlock(false)"
+              >Sign In</span
+            >
           </div>
         </form>
       </div>
@@ -67,11 +121,11 @@
 </template>
 
 <script>
-import { reactive, ref, computed, watchEffect } from 'vue'
-import axios from 'axios'
-import instance from '../api/axios'
-import router from '../router/index'
-import store from '../store/index'
+import { reactive, ref, computed, watchEffect } from 'vue';
+import axios from 'axios';
+import instance from '../api/axios';
+import router from '../router/index';
+import store from '../store/index';
 
 export default {
   name: 'Login',
@@ -81,33 +135,33 @@ export default {
     const isMatched = ref(true);
     const signUpState = reactive({
       form: {
-        loginId: "",
-        nickname: "",
-        password: "",
-        passwordConfirm: ""
+        loginId: '',
+        nickname: '',
+        password: '',
+        passwordConfirm: '',
       },
-      message: "",
+      message: '',
     });
 
     const signInState = reactive({
       form: {
-        loginId: "",
-        password: "",
-      }
-    })
+        loginId: '',
+        password: '',
+      },
+    });
 
     /**
      * 로그인한 사용자가 다시 로그인 할 수 없도록 라우팅
      */
     watchEffect(() => {
-      if(isAuthenticated.value) {
-        router.push({path: '/'});
+      if (isAuthenticated.value) {
+        router.push({ path: '/' });
       }
-    })
+    });
 
     /**
      * 로그인, 회원가입 창 전환
-     * @param status 
+     * @param status
      */
     const changeBlock = (status) => {
       isBlock.value = status;
@@ -115,42 +169,47 @@ export default {
 
     /**
      * 비밀번호 일치 여부 확인
-     * @param status 
+     * @param status
      */
     const matchedPassword = (status) => {
       isMatched.value = status;
-    }
+    };
 
     /**
      * 회원가입
      */
     const register = () => {
-      const isFilled = signUpState.form.loginId && signUpState.form.nickname && 
-      signUpState.form.password && signUpState.form.passwordConfirm;
+      const isFilled =
+        signUpState.form.loginId &&
+        signUpState.form.nickname &&
+        signUpState.form.password &&
+        signUpState.form.passwordConfirm;
 
-      if(!isFilled) {
+      if (!isFilled) {
         window.alert('모든 필드를 작성해주세요.');
         return;
       }
 
-      if(signUpState.form.password !== signUpState.form.passwordConfirm) {
+      if (signUpState.form.password !== signUpState.form.passwordConfirm) {
         matchedPassword(false);
         return;
       } else {
-        axios.post('/api/join', signUpState.form).then((res) => {
-          signUpState.message = res.data.message;
-          window.alert(signUpState.message);
-          changeBlock(false); // 로그인으로 이동
+        axios
+          .post('/api/join', signUpState.form)
+          .then((res) => {
+            signUpState.message = res.data.message;
+            window.alert(signUpState.message);
+            changeBlock(false); // 로그인으로 이동
+          })
+          .catch((error) => {
+            if (error.response && error.response.data) {
+              signUpState.message = error.response.data.message;
+            } else {
+              signUpState.message = '사용할 수 없는 아이디 입니다.';
+            }
 
-        }).catch((error) => {
-          if(error.response && error.response.data) {
-            signUpState.message = error.response.data.message;
-          } else {
-            signUpState.message = "사용할 수 없는 아이디 입니다.";
-          }
-
-          window.alert(signUpState.message);
-        });
+            window.alert(signUpState.message);
+          });
       }
     };
 
@@ -160,32 +219,37 @@ export default {
     const login = () => {
       const isFilled = signInState.form.loginId && signInState.form.password;
 
-      if(!isFilled) {
+      if (!isFilled) {
         window.alert('모든 필드를 작성해주세요.');
         return;
       }
 
-      axios.post('/api/login', signInState.form).then((res) => {
-        const accessToken = res.headers["authorization"];
-        
-        if(accessToken) {
-          localStorage.setItem('accessToken', accessToken);
-          window.alert('로그인 성공!');
+      axios
+        .post('/api/login', signInState.form)
+        .then((res) => {
+          const accessToken = res.headers['authorization'];
 
-          instance.get('/api/user').then(() => {
-            // 초기화 작업 수행
-            store.dispatch('initializeAuthentication');
-            router.push({path: '/'});
-          }).catch((error) => {
-            console.error('사용자 정보를 조회하는데 실패했습니다.', error);
-          });
+          if (accessToken) {
+            localStorage.setItem('accessToken', accessToken);
+            window.alert('로그인 성공!');
 
-        } else {
-          window.alert('로그인 토큰을 받아오는데 실패했습니다.');
-        }
-      }).catch(() => {
-        window.alert('아이디 혹은 비밀번호가 일치하지 않습니다..');
-      })
+            instance
+              .get('/api/user')
+              .then(() => {
+                // 초기화 작업 수행
+                store.dispatch('initializeAuthentication');
+                router.push({ path: '/' });
+              })
+              .catch((error) => {
+                console.error('사용자 정보를 조회하는데 실패했습니다.', error);
+              });
+          } else {
+            window.alert('로그인 토큰을 받아오는데 실패했습니다.');
+          }
+        })
+        .catch(() => {
+          window.alert('아이디 혹은 비밀번호가 일치하지 않습니다..');
+        });
     };
 
     return {
@@ -197,13 +261,12 @@ export default {
       changeBlock,
       matchedPassword,
       register,
-      login
-    }
-  }
-}
-  
+      login,
+    };
+  },
+};
 </script>
 
 <style>
-  @import '../assets/login.css'
+@import '../assets/login.css';
 </style>
