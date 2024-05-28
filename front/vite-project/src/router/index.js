7f076ca93be5aaae25334e92e62003b06b98f3d1
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-28 15:00:37 +0900

    feat: 댓글 작성 및 삭제 즉각 반영하도록 구현

--- front/vite-project/src/router/index.js
+++ front/vite-project/src/router/index.js
@@ -1,13 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Content from '../components/Content.vue';
-import Login from '../views/Login.vue';
-import Review from '../views/review/Review.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Content from '../components/Content.vue'
+import Login from '../views/Login.vue'
+import Review from '../views/review/Review.vue'
+import Write from '../views/review/Write.vue'
 
 const routes = [
   { path: '/', component: Content },
   { path: '/login', component: Login },
   { path: '/review/:id', component: Review },
-];
+  { path: '/write', component: Write },
+]
 
 const router = createRouter({
   history: createWebHistory(),
