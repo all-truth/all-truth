f7083f41b509b79a8a8bdbeca5a6c3aa6a31373b
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-29 17:07:58 +0900

    feat: 리뷰 목록 Vuex로 관리

--- front/vite-project/src/store/index.js
+++ front/vite-project/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
+import axios from '../api/axios'
 
 export default createStore({
   state: {
     isAuthenticated: false,
+    reviews: []
   },
   mutations: {
     setAuthentication(state, status) {
@@ -10,6 +12,12 @@ export default createStore({
     },
     logout(state) {
       state.isAuthenticated = false;
+    },
+    setReviews(state, reviews) {
+      state.reviews = reviews;
+    },
+    addReview(state, review) {
+      state.reviews.push(review);
     },
   },
   actions: {
@@ -26,5 +34,18 @@ export default createStore({
       localStorage.removeItem('accessToken');
       commit('setAuthentication', false);
     },
-  }
-})
+    fetchReviews({ commit }) {
+      axios.get('/api/reviews').then((res) => {
+        commit('setReviews', res.data);
+      }).catch((error) => {
+        console.log('리뷰 조회 중 에러가 발생했습니다. ', error);
+      });
+    },
+    addReview({ commit }, review) {
+      commit('addReview', review);
+    },
+  },
+  getters: {
+    reviews: (state) => state.reviews,
+  },
+});
